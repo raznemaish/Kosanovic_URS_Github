54d4c0682dc4e85daf5b4b4710e054892a792db9
„karlo <karlo.kosanovi0@gmail.com>
2025-06-10 10:47:58 +0200

    dodan blinky program

--- Kosanovic_URS_LVL10/Core/Src/main.c
+++ Kosanovic_URS_LVL10/Core/Src/main.c
@@ -94,6 +94,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
